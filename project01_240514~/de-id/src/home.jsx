9ba4ed963194bd9d82ccffa4ccefed4caf44a556
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-05-15 17:59:10 +0900

    .

---
+++ project01_240514~/de-id/src/home.jsx
@@ -0,0 +1,9 @@
+const home = () => {
+    return (
+      <div className="home">
+        <h2>home.jsx</h2>
+      </div>
+    );
+}
+  
+export default home;
---
+++ project01_240514~/de-id/src/login.js
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+      <div className="Login">
+        안녕하세요
+      </div>
+    );
+  }
+  
+  export default Login;
