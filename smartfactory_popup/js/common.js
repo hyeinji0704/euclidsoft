32a406df1a3ae7492f4820c0cf7aba17e2d112d1
hyein ji <hyein0704@gmail.com>
2025-02-10 23:18:27 +0900

    .

--- smartfactory_popup/js/common.js
+++ smartfactory_popup/js/common.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
     
-    const swiper = new Swiper('#popup-zone .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 3, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 0, /* 팝업과 팝업 사이 여백 */
+    const swiper = new Swiper('.slide_box .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
             480: {    
                 slidesPerView: 2,
+                spaceBetween: 18,
             },
             1024: {   
                 slidesPerView: 3,
+                spaceBetween: 20,
             }
         },
         centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
