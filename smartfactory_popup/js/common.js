742243275f428355e25807bada60203c08bdd681
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2025-02-11 10:16:55 +0900

    .

--- smartfactory_popup/js/common.js
+++ smartfactory_popup/js/common.js
@@ -1,29 +1,33 @@
 $(document).ready(function(){
-    
+    function updatePopupCount() {
+        const popupCount = swiper.slides.length - swiper.loopedSlides * 2; // Swiper가 가상으로 추가한 슬라이드 제외
+        $('.slide_page b').text(popupCount); // HTML에 반영
+    }
+        // 페이지 로드 시 팝업 개수 업데이트
+    $(document).ready(function() {
+        updatePopupCount();
+    });
+
+
     const swiper = new Swiper('.slide_box .swiper', { /* 팝업을 감싼는 요소의 class명 */
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
             480: {    
                 slidesPerView: 2,
-                spaceBetween: 18,
             },
             1024: {   
                 slidesPerView: 3,
-                spaceBetween: 20,
             }
         },
-        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        autoplay: {  /* 팝업 자동 실행 */
-            delay: 2500,
-            disableOnInteraction: true,
-        },
         navigation: {
             nextEl: '.slide_box .left-arrow',
             prevEl: '.slide_box .right-arrow',
         },
     });
+
+    
 
    
     // body에 .on_popup 클래스 추가 (첫 로딩 시)
@@ -72,4 +76,7 @@ $(document).ready(function(){
     if (getCookie('popup_closed')) {
         body.removeClass('on_popup'); // 쿠키가 있으면 팝업 닫음
     }
+
+
+    
 })
